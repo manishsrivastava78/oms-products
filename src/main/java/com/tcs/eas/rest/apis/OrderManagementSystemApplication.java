ac3f4b5cbeefeccf53597e94f03e730a3a9ac949
manishsrivastava78 <manishsrivastava78@gmail.com>
2021-09-27 10:56:47 +0530

    add CORS

--- src/main/java/com/tcs/eas/rest/apis/OrderManagementSystemApplication.java
+++ src/main/java/com/tcs/eas/rest/apis/OrderManagementSystemApplication.java
@@ -2,7 +2,9 @@ package com.tcs.eas.rest.apis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /**
  * 
  * @author 44745
@@ -17,4 +19,15 @@ public class OrderManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderManagementSystemApplication.class, args);
 	}
+	@Bean
+	public WebMvcConfigurer configurer(){
+		return new WebMvcConfigurer(){
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*")
+						.allowedOrigins("*");
+			}
+		};
+	}
+	
 }
