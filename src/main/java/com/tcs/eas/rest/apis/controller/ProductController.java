fb41f4f88541678f67be313f1fe02330fc07daeb
manishsrivastava78 <manishsrivastava78@gmail.com>
2021-10-09 00:40:53 +0530

    add cors

--- src/main/java/com/tcs/eas/rest/apis/controller/ProductController.java
+++ src/main/java/com/tcs/eas/rest/apis/controller/ProductController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,6 +32,7 @@ import com.tcs.eas.rest.apis.utility.Utility;
  */
 @RestController
 @RequestMapping("/apis/v1")
+@CrossOrigin(origins = "*",allowedHeaders = "*")
 public class ProductController {
 
 	@Autowired
